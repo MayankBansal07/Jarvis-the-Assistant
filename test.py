60b51d0a5ed28beb77eea31fef6037dd2823bff9
Mayank Bansal <143720231+MayankBansal07@users.noreply.github.com>
2025-07-11 18:46:22 +0530

    test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyDXij67zQf3y7nZfqz4JZLQIvkDFxznYwo")
+genai.configure(api_key="Type_API_key_here")
 model = genai.GenerativeModel("gemini-1.5-flash")
 response = model.generate_content("Who is Elon Musk?")
 print(response.text)
